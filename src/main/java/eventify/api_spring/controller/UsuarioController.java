9ae8a762ede46ead3d6f8ab7dd993e256d6d82c0
uken49 <heldergavioes@hotmail.com>
2023-04-17 14:35:36 -0300

    Criando e exibindo usuario

--- src/main/java/eventify/api_spring/controller/UsuarioController.java
+++ src/main/java/eventify/api_spring/controller/UsuarioController.java
@@ -2,24 +2,32 @@ package eventify.api_spring.controller;
 
 import eventify.api_spring.domain.Usuario;
 import eventify.api_spring.repository.UsuarioRepository;
+import eventify.api_spring.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
 // Controller recebe as requisições e as encaminha para o Service
 public class UsuarioController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Usuario>> exibir(@PathVariable Integer id) {
+        Optional<Usuario> resposta = UsuarioService.exibir(id);
 
-    @GetMapping
-    public List<Usuario> listar() {
-        return usuarioRepository.findAll();
+        if (resposta.isEmpty())
+            return ResponseEntity.status(409).build();
+
+        return ResponseEntity.status(200).body(resposta);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+        return ResponseEntity.status(201).body(UsuarioService.cadastrar(usuario));
     }
 
 }
--- src/main/java/eventify/api_spring/service/UsuarioService.java
+++ src/main/java/eventify/api_spring/service/UsuarioService.java
@@ -1,8 +1,24 @@
 package eventify.api_spring.service;
 
+import eventify.api_spring.domain.Usuario;
+import eventify.api_spring.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 // Classe que executa toda regra de negócio e retorna os resultados para Controller
 public class UsuarioService {
+
+    @Autowired
+    private static UsuarioRepository repository;
+    public static Optional<Usuario> exibir(Integer id){
+        return repository.findById(id);
+    }
+
+    public static Usuario cadastrar(Usuario usuario){
+        repository.save(usuario);
+        return usuario;
+    }
 }
