b7e59a07c41dbc96d4d36f683ce0a0d411301693
GuilhermeHenrique525 <guilherme.dias@sptech.school>
2023-04-23 17:23:34 -0300

    Adicionando a domain pesquisa e a controller pesquisa

---
+++ src/main/java/eventify/api_spring/api/controller/PesquisaController.java
@@ -0,0 +1,59 @@
+package eventify.api_spring.api.controller;
+
+import eventify.api_spring.domain.Buffet;
+import eventify.api_spring.domain.Pesquisa;
+import eventify.api_spring.service.BuffetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pesquisa")
+public class PesquisaController {
+    @Autowired
+    private BuffetService buffetService;
+
+    @GetMapping()
+    private ResponseEntity<List<Buffet>> buscarBuffetProximo(@RequestBody Pesquisa p) {
+        List<Buffet> buffets = buffetService.listar();
+        List<Buffet> buffetsFiltrados = new ArrayList<>();
+
+        for (int i = 0; i < buffets.size(); i++) {
+            boolean isDentro = true;
+
+            if (!buffets.get(i).getTiposEventos().contains(p.getTipoEvento())) {
+                isDentro = false;
+            }
+
+            if (!buffets.get(i).getFaixaEtarias().contains(p.getFaixaEtaria())) {
+                isDentro = false;
+            }
+
+            if (calcularDistancia(p.getLatitude(), p.getLongitude(), buffets.get(i).getEndereco().getLatitude(), buffets.get(i).getEndereco().getLongitude()) > 15) {
+                isDentro = false;
+            }
+
+            if (isDentro) {
+                buffetsFiltrados.add(buffets.get(i));
+            }
+        }
+        return ResponseEntity.ok().body(buffetsFiltrados);
+    }
+    public static double calcularDistancia(double lat1, double long1, double lat2, double long2) {
+        // raio médio da Terra em quilômetros
+        double raioTerra = 6371;
+
+        // Calcula a diferença de latitude e longitude entre os dois pontos e converte o resultado para radianos.
+        double difLat = Math.toRadians(lat2 - lat1);
+        double difLong = Math.toRadians(long2 - long1);
+
+        // Esta linha calcula um valor intermediário chamado a usando a fórmula de Haversine
+        double a = Math.sin(difLat / 2) * Math.sin(difLat / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(difLong / 2) * Math.sin(difLong / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distancia = raioTerra * c;
+        return distancia;
+    }
+}
---
+++ src/main/java/eventify/api_spring/domain/Pesquisa.java
@@ -0,0 +1,79 @@
+package eventify.api_spring.domain;
+
+import java.time.LocalDate;
+
+public class Pesquisa {
+
+    private String endereco;
+    private Integer faixaEtaria;
+    private Integer qtdPessoas;
+    private String tipoEvento;
+    private Integer orcamento;
+    private LocalDate dataEvento;
+    private Double latitude;
+    private Double longitude;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Integer getFaixaEtaria() {
+        return faixaEtaria;
+    }
+
+    public void setFaixaEtaria(Integer faixaEtaria) {
+        this.faixaEtaria = faixaEtaria;
+    }
+
+    public Integer getQtdPessoas() {
+        return qtdPessoas;
+    }
+
+    public void setQtdPessoas(Integer qtdPessoas) {
+        this.qtdPessoas = qtdPessoas;
+    }
+
+    public String getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public void setTipoEvento(String tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+
+    public Integer getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(Integer orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
