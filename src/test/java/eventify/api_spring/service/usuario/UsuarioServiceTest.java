fe27087bf7e8f2f447bc9b6a16a41c23640fe08f
uken49 <heldergavioes@hotmail.com>
2023-05-22 14:34:24 -0300

    add asserts

--- src/test/java/eventify/api_spring/service/usuario/UsuarioServiceTest.java
+++ src/test/java/eventify/api_spring/service/usuario/UsuarioServiceTest.java
@@ -1,19 +1,23 @@
 package eventify.api_spring.service.usuario;
 
 import eventify.api_spring.api.configuration.security.jwt.GerenciadorTokenJwt;
+import eventify.api_spring.domain.Usuario;
 import eventify.api_spring.dto.usuario.UsuarioCadastrarDTO;
 import eventify.api_spring.dto.usuario.UsuarioDevolverDTO;
 import eventify.api_spring.repository.UsuarioRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,18 +32,35 @@ class UsuarioServiceTest {
     private PasswordEncoder passwordEncoder;
     @Mock
     private UsuarioRepository usuarioRepository;
-    @Mock
+    @InjectMocks
     private UsuarioService usuarioService;
 
-    @Disabled("Ainda em construção")
-    @Test
-    public void deve_cadastar_um_usuario(){
-        final UsuarioCadastrarDTO requisicao = usuarioCadastrarDTOFactory();
+    @Captor
+    private ArgumentCaptor<Usuario> usuarioArgumentCaptor;
 
-        when(passwordEncoder.encode());
+    @Test
+    @Disabled("Ainda em construção")
+    void deve_cadastrar_um_usuario_com_os_dados_correto(){
+        final UsuarioCadastrarDTO requisicao = usuarioCadastrarDTOFactory();
+        final Usuario usuario = usuarioFactory();
+
+        when(usuarioRepository.save(usuarioArgumentCaptor.capture())).thenReturn(usuario);
 
         final UsuarioDevolverDTO resposta = usuarioService.cadastrar(requisicao);
-        assertEquals(requisicao.getNome(), resposta.getNome());
+
+        Usuario capture = usuarioArgumentCaptor.capture();
+
+        assertEquals(requisicao.getTipoUsuario(), capture.getTipoUsuario());
+        assertEquals(requisicao.getCpf(), capture.getCpf());
+        assertEquals(requisicao.getNome(), capture.getNome());
+        assertEquals(requisicao.getEmail(), capture.getEmail());
+        assertEquals(requisicao.getAtivo(), capture.isAtivo());
+        assertEquals(requisicao.getBanido(), capture.isBanido());
+        assertNotEquals(requisicao.getSenha(), capture.getSenha());
+
+        assertEquals(capture.getId(), resposta.getId());
+        assertEquals(capture.getNome(), resposta.getNome());
+        assertEquals(capture.getEmail(), resposta.getEmail());
     }
 
     public static UsuarioCadastrarDTO usuarioCadastrarDTOFactory(){
@@ -50,6 +71,18 @@ class UsuarioServiceTest {
                 "31509983015",
                 1
         );
+    }
+
+    public static Usuario usuarioFactory(){
+        Usuario usuario = new Usuario();
+
+        usuario.setNome("Gabriel Santos");
+        usuario.setEmail("gabriel@santos.com");
+        usuario.setAtivo(true);
+        usuario.setTipoUsuario(1);
+        usuario.setSenha("123456");
+        usuario.setCpf("31509983015");
+        return usuario;
     }
 
 }
