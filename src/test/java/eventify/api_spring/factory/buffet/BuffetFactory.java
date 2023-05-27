56902bcfdde3a50105fc0359c6ba970c04e0a54a
uken49 <heldergavioes@hotmail.com>
2023-05-27 17:16:36 -0300

    add: testes do método listar

--- src/test/java/eventify/api_spring/factory/buffet/BuffetFactory.java
+++ src/test/java/eventify/api_spring/factory/buffet/BuffetFactory.java
@@ -1,4 +1,35 @@
 package eventify.api_spring.factory.buffet;
 
+import eventify.api_spring.domain.Agenda;
+import eventify.api_spring.domain.Buffet;
+import eventify.api_spring.factory.usuario.UsuarioFactory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class BuffetFactory {
+
+    public static Buffet buffet(){
+        final Buffet buffet = new Buffet();
+        buffet.setId(1);
+        buffet.setNome("Giussepe Cadura");
+        buffet.setAgendas(agenda(buffet));
+        buffet.setUsuario(UsuarioFactory.usuario());
+
+        return buffet;
+    }
+
+    private static List<Agenda> agenda(Buffet buffet){
+
+        Agenda agenda = new Agenda();
+
+        LocalDateTime data = LocalDateTime.of(2022,11,25,16,44,2);
+
+        agenda.setId(1);
+        agenda.setData(data);
+        agenda.setBuffet(buffet);
+
+        return List.of(agenda);
+    }
 }
--- src/test/java/eventify/api_spring/service/buffet/BuffetServiceTest.java
+++ src/test/java/eventify/api_spring/service/buffet/BuffetServiceTest.java
@@ -1,17 +1,27 @@
 package eventify.api_spring.service.buffet;
 
+import eventify.api_spring.domain.Buffet;
+import eventify.api_spring.dto.BuffetDtoResposta;
+import eventify.api_spring.factory.buffet.BuffetFactory;
 import eventify.api_spring.repository.BuffetRepository;
 import eventify.api_spring.repository.EventoRepository;
 import eventify.api_spring.repository.ImagemRepository;
 import eventify.api_spring.repository.UsuarioRepository;
 import eventify.api_spring.service.BuffetService;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -23,12 +33,26 @@ public class BuffetServiceTest {
     @Mock
     private ImagemRepository imagemRepository;
     @Mock
-    private UsuarioRepository usuarioRepository;
-    @Mock
     private EntityManager entityManager;
     @InjectMocks
     private BuffetService service;
 
+    @Test
+    void deve_retornar_uma_lista_vazia(){
 
+        when(buffetRepository.findAll()).thenReturn(Collections.emptyList());
+        final List<BuffetDtoResposta> lista = service.listar();
 
+        assertTrue(lista.isEmpty());
+    }
+
+    @Test
+    void deve_retornar_dois_buffets(){
+        final Buffet buffet = BuffetFactory.buffet();
+
+        when(buffetRepository.findAll()).thenReturn(List.of(buffet, buffet));
+        final List<BuffetDtoResposta> listar = service.listar();
+
+        assertEquals(2, listar.size());
+    }
 }
