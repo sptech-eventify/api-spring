469fe7617723515342d3037acd83b73f52dddaa6
LeoVasc5 <leovasc5@hotmail.com>
2023-10-20 20:03:44 -0300

    feature/dashboards

--- src/main/java/eventify/api_spring/repository/AgendaRepository.java
+++ src/main/java/eventify/api_spring/repository/AgendaRepository.java
@@ -2,6 +2,7 @@ package eventify.api_spring.repository;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,5 +16,5 @@ public interface AgendaRepository extends JpaRepository<Agenda, Integer> {
     @Query("SELECT a FROM Agenda a WHERE a.buffet.id = :idBuffet AND DATE(a.data) = :data")
     Optional<Agenda> findByBuffetIdAndDataEquals(@Param("idBuffet") Integer idBuffet, @Param("data") LocalDate data);
 
-    // Agenda findByBuffetIdAndDataEquals(Integer idBuffet, LocalDateTime data);
+    List<Agenda> findByBuffetId(Integer idBuffet);
 }
